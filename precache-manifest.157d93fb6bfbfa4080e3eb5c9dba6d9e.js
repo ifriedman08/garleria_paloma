27786e264cea35273bff176fd498b6fa52b7b9c0
Ivan <ivan@Ivans-MacBook-Pro.local>
2020-05-10 18:07:05 -0700

    Updates

---
+++ precache-manifest.157d93fb6bfbfa4080e3eb5c9dba6d9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16dc0510986b355a1498b3ae31c5ca3",
+    "url": "/galeria_paloma/index.html"
+  },
+  {
+    "revision": "a0fd75f266dd18134064",
+    "url": "/galeria_paloma/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "67bfe7bbdc40f9802164",
+    "url": "/galeria_paloma/static/css/main.360809f9.chunk.css"
+  },
+  {
+    "revision": "a0fd75f266dd18134064",
+    "url": "/galeria_paloma/static/js/2.2a25a99b.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/galeria_paloma/static/js/2.2a25a99b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67bfe7bbdc40f9802164",
+    "url": "/galeria_paloma/static/js/main.6b8868c6.chunk.js"
+  },
+  {
+    "revision": "e840dd18c6b5fb9a8fad",
+    "url": "/galeria_paloma/static/js/runtime-main.192ec273.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/garleria_paloma/precache-manifest.b9d1bbca9cee21b4d6f8fcf0189c51e9.js"
+  "/galeria_paloma/precache-manifest.157d93fb6bfbfa4080e3eb5c9dba6d9e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/garleria_paloma/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/galeria_paloma/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
